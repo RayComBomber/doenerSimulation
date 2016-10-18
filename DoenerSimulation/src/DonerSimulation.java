02051811f99553413261d4a94d013d43aa392f10
oechslerainer <ra.oechsle@gmail.com>
2016-10-18 19:33:31 +0200

    Adjusted the simulation parameters due to the exercise description.

--- DoenerSimulation/src/DonerSimulation.java
+++ DoenerSimulation/src/DonerSimulation.java
@@ -3,29 +3,27 @@ import java.util.concurrent.ThreadLocalRandom;
 public class DonerSimulation {
 
 	private static final int EMPLOYEE_COUNT = 3;
-	private static final int CUSTOMER_COUNT_MIN = 3;
-	private static final int CUSTOMER_COUNT_MAX = 6;
-	private static final int TIME_BETWEEN_GROUPS_MIN = 3000;
-	private static final int TIME_BETWEEN_GROUPS_MAX = 6000;
+	private static final int GROUP_COUNT = 20;
+	private static final int CUSTOMER_COUNT_MIN = 2;
+	private static final int CUSTOMER_COUNT_MAX = 5;
+	private static final int TIME_BETWEEN_GROUPS = 5000;
 	
 	
 	public static void main(String[] args) {
 		System.out.println("Döner Simulation:");
 		
-		// One time init
+		// Init
 		DoenerStore store = new DoenerStore(EMPLOYEE_COUNT);
 		
-		
-		// Infinite loop
-		for(int groupId = 0; true; groupId++){
+		// Spawn Groups
+		for(int groupId = 0; groupId < GROUP_COUNT; groupId++){
 			
 			int customerCount = ThreadLocalRandom.current().nextInt(CUSTOMER_COUNT_MIN, CUSTOMER_COUNT_MAX + 1);
+			System.out.println("Group " + groupId + " with " + customerCount + " customers has arrived.");
 			new CustomerGroup(groupId, customerCount, store);
-			
-			
-			int timeUntilNextGroupArrives = ThreadLocalRandom.current().nextInt(TIME_BETWEEN_GROUPS_MIN, TIME_BETWEEN_GROUPS_MAX + 1);
+						
 			try {
-				Thread.sleep(timeUntilNextGroupArrives);
+				Thread.sleep(TIME_BETWEEN_GROUPS);
 			} catch (InterruptedException e) {}
 		}
 				
